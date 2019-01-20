a9758e16474049fc0834e38e02782cf9796852f7
USTC-Han <839057430@qq.com>
2019-01-20 19:14:16 +0800

    add html and sql

--- index.js
+++ index.js
@@ -1,34 +1,68 @@
 const express = require('express');
-const app = express();
-const articles = [{title:'example'}];
 const bodyParser = require('body-parser');
+const app = express();
+const Article = require('./db').Article;
+const read = require('node-readability');
+
 app.set('port', process.env.PORT || 3000);
 
-app.get('/articles', (req, res, next) => {
-    res.send('articles');
-});
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extend: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/articles',(req, res, next) =>{
-    const article = {title:req.body.title};
-    articles.push(article);
-    res.send(article);
+app.use(
+    '/css/bootstrap.css',
+    express.static('node_modules/bootstrap/dist/css/bootstrap.css')
+);
+
+app.get('/articles', (req, res, next) => {
+    Article.all((err, articles) => {
+        if (err) return next(err);
+
+        res.format({
+            html: () => {
+                res.render('articles.ejs', { articles });
+            },
+            json: () => {
+                res.send(articles);
+            }
+        });
+    });
 });
 
 app.get('/articles/:id', (req, res, next) => {
     const id = req.params.id;
-    console.log('Fetching:', id);
-    res.send(articles[id]);
+    Article.find(id, (err, article) => {
+        if (err) return next(err);
+        res.send(article);
+    });
 });
 
-app.delete('articles/:id', (req, res, next) => {
+app.delete('/articles/:id', (req, res, next) => {
     const id = req.params.id;
-    console.log('delete', id);
-    res.send(articles[id]);
+    Article.delete(id, (err) => {
+        if (err) return next(err);
+        res.send({ message: 'Deleted' });
+    });
+});
+
+app.post('/articles', (req, res, next) => {
+    const url = req.body.url;
+
+    read(url, (err, result) => {
+        if (err || !result) res.status(500).send('Error downloading article');
+        Article.create(
+            { title: result.title, content: result.content },
+            (err, article) => {
+                if (err) return next(err);
+                console.log(article);
+                res.send('OK');
+            }
+        );
+    });
 });
 
 app.listen(app.get('port'), () => {
     console.log('App started on port', app.get('port'));
 });
+
+module.exports = app;
